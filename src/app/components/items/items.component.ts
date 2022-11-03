42da59aad37ff1ef6ef458e020427fde88e80ce6
Chema Torres <chemaaa94@gmail.com>
2022-11-03 10:49:23 -0600

    primeros pasos

---
+++ src/app/components/items/items.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {Item} from '../../models/item';
+
+@Component({
+  selector: 'app-items',
+  templateUrl: './items.component.html',
+  styleUrls: ['./items.component.css']
+})
+export class ItemsComponent implements OnInit {
+
+  items: Item[] = [];
+  constructor() { }
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        id: 0,
+        title: 'manzana',
+        price: 10.5,
+        quantity: 4,
+        completed: false,
+      },
+      {
+        id: 1,
+        title: 'Mango',
+        price: 9.99,
+        quantity: 2,
+        completed: true
+      }
+    ]
+  }
+
+  deleteItem(item:Item){
+    this.items = this.items.filter(x => x.id !== item.id)
+  }
+}
